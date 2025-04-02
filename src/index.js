5400c44be6ba24e8fc84f0749226c2cee3bbc7f5
mrthinh307 <23020708@vnu.edu.vn>
2025-04-03 02:09:38 +0700

    style: add base styles and fonts for the application

--- src/index.js
+++ src/index.js
@@ -1,11 +1,14 @@
 import React from 'react' // nạp thư viện react
 import ReactDOM from 'react-dom' // nạp thư viện react-dom
+import './assets/styles/base.css' // nạp file css base.css
+import './assets/styles/fonts.css' // nạp file css fonts.css
 
 // Tạo component App
 function App() {
     return (
         <div>
             <h1>Back-market!</h1>
+            <p>Refurbished tech that's better for the planet | Back Market</p>
         </div>
     )
 }
