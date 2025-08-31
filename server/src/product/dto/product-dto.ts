ddfedcb7f8f6f73df9e551f4b60709f4481ae16f
mrthinh307 <23020708@vnu.edu.vn>
2025-09-01 00:42:44 +0700

    feat(server/product): implement product list retrieval with filtering options

---
+++ server/src/product/dto/product-dto.ts
@@ -0,0 +1,49 @@
+import { IsOptional, IsNumberString, IsBoolean, IsUUID } from 'class-validator';
+import { Transform } from 'class-transformer';
+
+export class GetProductListQueryDto {
+  @IsNumberString()
+  categoryId: string; // Will be converted to number in service
+
+  @IsOptional()
+  @IsUUID()
+  brandId?: string;
+
+  @IsOptional()
+  @Transform(({ value }) => {
+    if (value === 'true') return true;
+    if (value === 'false') return false;
+    return false; // default to false
+  })
+  @IsBoolean()
+  isExcludedBrand?: boolean = false;
+}
+export class ProductListDto {
+  products: ({
+    id: string;
+    title: string;
+    brand: {
+      id: string;
+      name: string;
+    };
+    category: {
+      id: string;
+      name: string;
+    };
+    image: string | null; // Allow null for now
+    color: string;
+    price: number;
+    priceWithCurrency: `$${number}`;
+    stock: number;
+    variants: {
+      colors: {
+        name: string;
+        // value: string;
+      }[]; // 👈 Array of color variants
+    };
+    reviewRating: {
+      count: number;
+      average: number;
+    };
+  })[];
+}
